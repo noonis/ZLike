9ee22972c4fc4cf8bf83d4e9a59500ada2d04bfe
noonis <anis.laraba@gmx.com>
2018-01-26 10:20:46 +0100

    Start

---
+++ src/Jeu/JeuVideo/Application.java
@@ -0,0 +1,18 @@
+package Jeu.JeuVideo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(1900, 1600);
+        frame.setVisible(true);
+        System.out.println();
+    }
+}
